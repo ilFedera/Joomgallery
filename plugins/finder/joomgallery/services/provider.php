fc3c54c4579e78779537466fe911853513851a0f
19leunam93 <tech.spuur@quickline.ch>
2026-05-01 11:53:42 +0200

    fix code-style

--- plugins/finder/joomgallery/services/provider.php
+++ plugins/finder/joomgallery/services/provider.php
@@ -3,7 +3,7 @@
  * *********************************************************************************
  *    @package    com_joomgallery                                                 **
  *    @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>          **
- *    @copyright  2008 - 2025  JoomGallery::ProjectTeam                           **
+ *    @copyright  2008 - 2026  JoomGallery::ProjectTeam                           **
  *    @license    GNU General Public License version 3 or later                   **
  * *********************************************************************************
  */
